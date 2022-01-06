40aaf59bd3e855e3b1cc85c8485eabfaeb9fafd0
Leandro <leandro.n.aguilera@gmail.com>
2022-01-05 22:18:38 -0300

    Promise.all

--- src/index.js
+++ src/index.js
@@ -20,8 +20,20 @@ const heroeId2 = 'iron';
 
 // }); 
 
-buscarHeroe( heroeId1 ).then( heroe => {
-    console.log(`Enviando a ${heroe.nombre} a la mision`);
-})
+// buscarHeroe( heroeId1 ).then( heroe => {
+
+//     // console.log(`Enviando a ${heroe.nombre} a la mision`);
+//     buscarHeroe( heroeId2 ).then( heroe2 => {
+
+//         console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión`)
+
+//     });
+// });
+
+Promise.all( [buscarHeroe(heroeId1), buscarHeroe(heroeId2)] )
+    
+    .then( ([heroe1, heroe2]) => {
+    console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión`)
+});
 
 console.log('Fin del programa.')
