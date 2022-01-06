0d6778aa0847ca3f6570272954277429f1135805
Leandro <leandro.n.aguilera@gmail.com>
2022-01-06 07:58:12 -0300

    Await

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-// import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas';
-import './styles.css';
-import { buscarHeroe, buscarHeroeAsync } from './js/promesas';
+import { obtenerHeroesArr } from "./js/await";
 
-// // promesaLenta.then( console.log );
-// // promesaMedia.then( console.log );
-// // promesaRapida.then( console.log );
-
-// Promise.race( [promesaLenta, promesaMedia, promesaRapida] )
-//         .then( console.log )
-//         .catch( console.warn)
-
-
-buscarHeroe( 'capi1' )
-        .then( heroe => console.log(heroe) )
-        .catch( console.warn );
-
-
-buscarHeroeAsync( 'iron' )
-        .then( heroe => console.log(heroe) )
-        .catch( console.warn )
+obtenerHeroesArr().then( console.table )
